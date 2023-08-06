fb5907269f7d85ede2eab3d2c92e1e7edb96f833
Lieslot <rlaxodud1999@gmail.com>
2023-08-06 13:01:41 +0900

    vector

---
+++ Vector.h
@@ -0,0 +1,134 @@
+template<typename T> 
+class Vector {
+
+
+
+	private:
+		int capacity;
+		int len;
+		T* array;
+		
+	
+	public:
+
+		Vector() : capacity(0), array(nullptr), len(0){}
+
+		Vector(int count, T initValue = T()) : array(new T[count]), capacity(count), len(count) {
+
+			for (int i = 0; i < count; i++) {
+				this->array[i] = initValue;
+			}
+
+		}
+		~Vector() {
+			if (capacity > 0) {
+				delete[] array;
+				array = nullptr;
+			}
+		}
+
+
+
+		void arrayCopy(T dest[], T src[], int startIndex) {
+			for (int i = startIndex,  j = 0; j < len; i++, j++) {
+				dest[i] = src[j];
+			} 
+		}
+
+		bool isEmpty() {
+			return len == 0;
+		}
+
+		void resize(int newSize, int startIndex = 0) {
+			T* temp = new T[newSize];
+				
+			arrayCopy(temp, array, startIndex);
+
+
+			delete[] array;
+			array = nullptr;
+			array = temp;
+
+			capacity = newSize;
+			len = std::min(len, newSize);
+		}
+
+		void pushBack(T value) {
+			
+			if (capacity == 0) {
+				array = new T[1];
+				capacity++;
+
+			}
+
+
+			if (capacity == len) {
+				resize(capacity*2, 0);
+			}
+
+				
+			array[len++] = value;
+		
+			
+		}
+
+	
+
+		void popBack() {
+			try {
+				if (this->isEmpty() == true) throw len; 
+			}
+
+			catch (int size){
+				std::cout << "the array is empty\n";
+			}
+		}
+		
+		void pushFront(T value) {
+			if (capacity == len) {
+				resize(capacity*2, 1);
+				
+			}
+			else if (capacity > len) {
+				for (int i = capacity-1; i >= 0; i--) {
+					array[i+1] = array[i];
+				}
+			}
+			len++;
+			array[0] = value;
+		}
+
+		void popFront(T value) {
+			for (int i = capacity-2; i >= 0; i--) {
+					array[i] = array[i+1];
+			}
+			len--;
+		}
+		
+		int size() {
+			return len;
+		}
+  
+		T operator[](int index) const { 
+			return array[index]; 
+		}
+		T& operator[](int index) {
+			return array[index];
+		}
+
+		T* begin() {
+			return array;
+		}
+		T* end() {
+			return nullptr;
+		}
+
+
+
+
+
+
+
+
+		
+};
