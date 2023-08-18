b3cd717e998301a54dd1760953ff204a796b76b0
Lieslot <rlaxodud1999@gmail.com>
2023-08-18 23:00:35 +0900

    1.Priority_Queue.h 생성 2. Vector.h 수정 -iterator 작동오류 -pushBack 오류 수정

--- PriorityQueue.h
+++ PriorityQueue.h
@@ -1,3 +1,4 @@
+
 template<typename T>
 class VectorIterator {
 
@@ -132,15 +133,15 @@ public:
 			resize(capacity * 2, 1);
 
 		}
-
-		for (int i = capacity - 1; i >= 0; i--) {
-			array[i + 1] = array[i];
-		
+		else if (capacity > len) {
+			for (int i = capacity - 1; i >= 0; i--) {
+				array[i + 1] = array[i];
+			}
+		}
 		len++;
 		array[0] = value;
-
-		}
 	}
+
 	void popFront(T value) {
 		for (int i = capacity - 2; i >= 0; i--) {
 			array[i] = array[i + 1];
@@ -175,3 +176,129 @@ public:
 
 
 };
+
+
+
+
+template<typename T>
+void swap(T& a, T& b) {
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+
+
+
+template<typename T>
+class PriorityQueue {
+	
+
+
+private:
+	Vector<T> data;
+	int len;
+
+public:
+	
+	PriorityQueue<T>() {
+		len = 0;
+		data.pushBack(T());
+
+
+	}
+
+	void push(T value) {
+
+
+		data.pushBack(value);
+		len++;
+
+		int curPosition = len;
+
+		while (curPosition != 1) {
+			int parent = curPosition / 2;
+
+			if (data[parent] > data[curPosition]) break;
+			
+			swap(data[parent], data[curPosition]);
+
+			curPosition = parent;
+			
+		}
+		
+	}
+
+	void pop() {
+		if (len == 0) {
+			
+			return;
+
+		}
+
+		swap(data[1], data[len]);
+		len--;
+
+		data.popBack();
+
+		if (len == 0) return;
+
+		int curPosition = 1;
+
+		while (curPosition*2 <= len) {
+			int leftIndex = curPosition * 2;
+			int rightIndex = curPosition * 2+1;
+
+			int nxtIndex = leftIndex;
+			if (rightIndex <=  len && data[rightIndex] > data[leftIndex]) {
+
+
+				nxtIndex = rightIndex;
+
+		
+			}
+		
+
+			if (data[nxtIndex] > data[curPosition]) {
+					swap(data[curPosition], data[nxtIndex]);
+					curPosition = nxtIndex;
+			}
+			else {
+					break;
+
+			}
+		
+			
+			
+
+		}
+		
+		
+		
+		
+
+
+		
+	}
+
+	T top() {
+		return data[1];
+	}
+
+	int size() {
+		return len;
+	}
+
+	bool isEmpty() {
+		return this->size() == 0;
+	}
+
+	
+	
+	
+	
+
+
+};
+
+
